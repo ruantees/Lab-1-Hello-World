3bdee5bbca6606574762bf31866051b6b5ab15ee
Vartotojas <ruant142@gmail.com>
2023-09-27 15:12:42 +0300

    Added the "Change text" button

--- app/src/main/java/com/example/lab1helloworld/MainActivity.kt
+++ app/src/main/java/com/example/lab1helloworld/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.lab1helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val displayedText = findViewById<TextView>(R.id.displayedText)
+        val buttonTextChanger = findViewById<Button>(R.id.buttonTextChanger)
+
+        buttonTextChanger.setOnClickListener {
+            displayedText.text = getString(R.string.changed_text)
+        }
     }
 }
