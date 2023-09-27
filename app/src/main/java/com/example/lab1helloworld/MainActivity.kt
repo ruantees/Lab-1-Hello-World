0054d460474657d1d80aa333c5ba95d66f4ba0ff
Vartotojas <ruant142@gmail.com>
2023-09-27 15:18:41 +0300

    Added the "Change color" button

--- app/src/main/java/com/example/lab1helloworld/MainActivity.kt
+++ app/src/main/java/com/example/lab1helloworld/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.lab1helloworld
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,9 +13,14 @@ class MainActivity : AppCompatActivity() {
 
         val displayedText = findViewById<TextView>(R.id.displayedText)
         val buttonTextChanger = findViewById<Button>(R.id.buttonTextChanger)
+        val buttonColorChanger = findViewById<Button>(R.id.buttonColorChanger)
 
         buttonTextChanger.setOnClickListener {
             displayedText.text = getString(R.string.changed_text)
+        }
+
+        buttonColorChanger.setOnClickListener {
+            displayedText.setTextColor(Color.parseColor("#008000"))
         }
     }
 }
